1fbba5f446424c0f02f95a23e74fbee7a598fd20
zhangjian <zhangjian863516@163.com>
2020-12-11 01:47:45 +0800

    添加vec3 color

--- RayTracing/RayTracing/RayTracing.cpp
+++ RayTracing/RayTracing/RayTracing.cpp
@@ -1,5 +1,7 @@
 ﻿// RayTracing.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 //
+#include "color.h"
+#include "vec3.h"
 
 #include <iostream>
 
@@ -17,15 +19,8 @@ int main()
     for (int j = image_height - 1; j >= 0; --j) {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
-            auto r = double(i) / (image_width - 1);
-            auto g = double(j) / (image_height - 1);
-            auto b = 0.25;
-
-            int ir = static_cast<int>(255.999 * r);
-            int ig = static_cast<int>(255.999 * g);
-            int ib = static_cast<int>(255.999 * b);
-
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            color pixel_color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0.25);
+            write_color(std::cout, pixel_color);
         }
     }
     std::cerr << "\nDone.\n";
