f8f89adf58b01c8f1d16e1768d3db06a1cc13f5d
zhangjian <zhangjian863516@163.com>
2020-12-12 23:02:18 +0800

    7.2 Generating Pixels with Multiple Samples

--- RayTracing/RayTracing/rtweekend.h
+++ RayTracing/RayTracing/rtweekend.h
@@ -2,7 +2,7 @@
 #include <cmath>
 #include <limits>
 #include <memory>
-
+#include <cstdlib>
 
 // Usings
 
@@ -25,3 +25,27 @@ inline double degrees_to_radians(double degrees) {
 
 #include "ray.h"
 #include "vec3.h"
+
+//inline double random_double() {
+//    // Returns a random real in [0,1).
+//    return rand() / (RAND_MAX + 1.0);
+//}
+
+#include <random>
+
+inline double random_double() {
+    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    static std::mt19937 generator;
+    return distribution(generator);
+}
+
+inline double random_double(double min, double max) {
+    // Returns a random real in [min,max).
+    return min + (max - min) * random_double();
+}
+
+inline double clamp(double x, double min, double max) {
+    if (x < min) return min;
+    if (x > max) return max;
+    return x;
+}
