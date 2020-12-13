9d4ec6fe9cfa415412a717fe417111908161630c
zhangjian <zhangjian863516@163.com>
2020-12-14 01:30:00 +0800

    4.3 A Checker Texture

--- RayTracing/RayTracing/material.h
+++ RayTracing/RayTracing/material.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "rtweekend.h"
 #include "ray.h"
+#include "texture.h"
+
 struct hit_record;
 
 class material {
@@ -12,7 +14,9 @@ public:
 
 class lambertian : public material {
 public:
-    lambertian(const color& a) : albedo(a) {}
+    //lambertian(const color& a) : albedo(a) {}
+    lambertian(const color& a) : albedo(make_shared<solid_color>(a)) {}
+    lambertian(shared_ptr<texture> a) : albedo(a) {}
 
     virtual bool scatter(
         const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
@@ -25,12 +29,14 @@ public:
 
         //scattered = ray(rec.p, scatter_direction);
         scattered = ray(rec.p, scatter_direction, r_in.time());
-        attenuation = albedo;
+        //attenuation = albedo;
+        attenuation = albedo->value(rec.u, rec.v, rec.p);
         return true;
     }
 
 public:
-    color albedo;
+    //color albedo;
+    shared_ptr<texture> albedo;
 };
 
 class metal : public material {
