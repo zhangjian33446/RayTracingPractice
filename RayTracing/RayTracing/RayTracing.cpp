154ab5b83425e161f2e4d47f4c004748fd75bc8a
zhangjian <zhangjian863516@163.com>
2020-12-13 20:19:34 +0800

    12.2 Generating Sample Rays

--- RayTracing/RayTracing/RayTracing.cpp
+++ RayTracing/RayTracing/RayTracing.cpp
@@ -81,7 +81,13 @@ int main()
     //world.add(make_shared<sphere>(point3(R, 0, -1), R, material_right));
 
     // Camera
-    camera cam(point3(-2, 2, 1), point3(0, 0, -1), vec3(0, 1, 0), 20, aspect_ratio);
+    point3 lookfrom(3, 3, 2);
+    point3 lookat(0, 0, -1);
+    vec3 vup(0, 1, 0);
+    auto dist_to_focus = (lookfrom - lookat).length();
+    auto aperture = 2.0;
+
+    camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus); // 看下小孔成像的原理
 
     // Render
 
