2402d9ba888efc704f56f6910f1ca7fc246ca5f3
zhangjian <zhangjian863516@163.com>
2020-12-16 00:47:21 +0800

    8 Instances

--- RayTracing/RayTracing/RayTracing.cpp
+++ RayTracing/RayTracing/RayTracing.cpp
@@ -16,6 +16,7 @@
 #include "perlin.h"
 #include "aabb.h"
 #include "aarect.h"
+#include "box.h"
 
 //double hit_sphere(const point3& center, double radius, const ray& r) {
 //    vec3 oc = r.origin() - center;
@@ -45,6 +46,9 @@ hittable_list cornell_box() {
     objects.add(make_shared<xz_rect>(0, 555, 0, 555, 0, white));
     objects.add(make_shared<xz_rect>(0, 555, 0, 555, 555, white));
     objects.add(make_shared<xy_rect>(0, 555, 0, 555, 555, white));
+
+    objects.add(make_shared<box>(point3(130, 0, 65), point3(295, 165, 230), white));
+    objects.add(make_shared<box>(point3(265, 0, 295), point3(430, 330, 460), white));
 
     return objects;
 }
