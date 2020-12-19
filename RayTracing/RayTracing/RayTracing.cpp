81fb4be17f30f7be0611fc6c8204338338de00aa
zhangjian <zhangjian863516@163.com>
2020-12-19 17:40:50 +0800

    2.2 Showing Convergence

--- RayTracing/RayTracing/RayTracing.cpp
+++ RayTracing/RayTracing/RayTracing.cpp
@@ -448,14 +448,19 @@ color ray_color(const ray& r, const color& background, const hittable& world, in
 #include <stdlib.h>
 
 int main() {
-    int N = 1000;
     int inside_circle = 0;
-    for (int i = 0; i < N; i++) {
+    int runs = 0;
+    std::cout << std::fixed << std::setprecision(12);
+    while (true) {
+        runs++;
         auto x = random_double(-1, 1);
         auto y = random_double(-1, 1);
         if (x * x + y * y < 1)
             inside_circle++;
+
+        if (runs % 100000 == 0)
+            std::cout << "Estimate of Pi = "
+            << 4 * double(inside_circle) / runs
+            << '\n';
     }
-    std::cout << std::fixed << std::setprecision(12);
-    std::cout << "Estimate of Pi = " << 4 * double(inside_circle) / N << '\n';
 }
