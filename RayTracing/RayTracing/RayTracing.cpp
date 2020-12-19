08b0d200e7b2032380eea733a116b61d72904f01
zhangjian <zhangjian863516@163.com>
2020-12-19 18:58:55 +0800

    3.3 Constructing a PDF

--- RayTracing/RayTracing/RayTracing.cpp
+++ RayTracing/RayTracing/RayTracing.cpp
@@ -447,13 +447,17 @@ color ray_color(const ray& r, const color& background, const hittable& world, in
 #include <math.h>
 #include <stdlib.h>
 
+inline double pdf(double x) {
+    return 0.5 * x;
+}
+
 int main() {
     int N = 1000000;
     auto sum = 0.0;
     for (int i = 0; i < N; i++) {
-        auto x = random_double(0,2);
-        sum += x*x;
+        auto x = sqrt(random_double(0, 4));
+        sum += x * x / pdf(x);
     }
     std::cout << std::fixed << std::setprecision(12);
-    std::cout << "I = " << 2 * sum/N << '\n';
+    std::cout << "I = " << sum / N << '\n';
 }
