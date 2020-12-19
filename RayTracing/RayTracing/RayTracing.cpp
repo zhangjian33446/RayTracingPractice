74b8277fde344ad2cbbf7a5cc2dd7d1261cac296
zhangjian <zhangjian863516@163.com>
2020-12-19 19:01:10 +0800

    3.4 Importance Sampling

--- RayTracing/RayTracing/RayTracing.cpp
+++ RayTracing/RayTracing/RayTracing.cpp
@@ -448,14 +448,14 @@ color ray_color(const ray& r, const color& background, const hittable& world, in
 #include <stdlib.h>
 
 inline double pdf(double x) {
-    return 0.5 * x;
+    return 0.5;
 }
 
 int main() {
     int N = 1000000;
     auto sum = 0.0;
     for (int i = 0; i < N; i++) {
-        auto x = sqrt(random_double(0, 4));
+        auto x = random_double(0, 2);
         sum += x * x / pdf(x);
     }
     std::cout << std::fixed << std::setprecision(12);
