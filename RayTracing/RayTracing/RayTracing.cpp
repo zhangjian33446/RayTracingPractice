0be40fc68ec298d61781aa645f96677e15bfe3c3
zhangjian <zhangjian863516@163.com>
2020-12-19 19:02:50 +0800

    4. MC Integration on the Sphere of Directions

--- RayTracing/RayTracing/RayTracing.cpp
+++ RayTracing/RayTracing/RayTracing.cpp
@@ -447,16 +447,17 @@ color ray_color(const ray& r, const color& background, const hittable& world, in
 #include <math.h>
 #include <stdlib.h>
 
-inline double pdf(double x) {
-    return 0.5;
+inline double pdf(const vec3& p) {
+    return 1 / (4 * pi);
 }
 
 int main() {
     int N = 1000000;
     auto sum = 0.0;
     for (int i = 0; i < N; i++) {
-        auto x = random_double(0, 2);
-        sum += x * x / pdf(x);
+        vec3 d = random_unit_vector();
+        auto cosine_squared = d.z() * d.z();
+        sum += cosine_squared / pdf(d);
     }
     std::cout << std::fixed << std::setprecision(12);
     std::cout << "I = " << sum / N << '\n';
